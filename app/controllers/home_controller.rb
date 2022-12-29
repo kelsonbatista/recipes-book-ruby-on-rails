34663153d02ea5b4c57764dd8cea2860732365ea
kelsonbatista <kelsonbatista@gmail.com>
2022-12-29 11:51:31 -0300

    feat(recipe): basic class

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,12 @@
 class HomeController < ApplicationController
   def index
+    r1 = Recipe.new('Bolo de cenoura', 'Sobremesa')
+    r2 = Recipe.new('Feijoada', 'Prato principal')
+    r3 = Recipe.new('Bolo de chocolate', 'Sobremesa')
+
+    @recipes = [r1, r2, r3]
   end
-  
+
   def ola
   end
 end
---
+++ app/models/recipe.rb
@@ -0,0 +1,8 @@
+class Recipe
+  attr_reader :name, :type
+
+  def initialize(name, type)
+    @name = name
+    @type = type
+  end
+end
